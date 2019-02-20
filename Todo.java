89b7137f72919e8b7a3d86edf85d3e5b2dcc4d55
bstick2 <47426528+bstick2@users.noreply.github.com>
2019-02-19 23:50:00 -0600

    Add files via upload

---
+++ Todo.java
@@ -0,0 +1,27 @@
+public class Todo {/*
+Lab Section_________: 11
+Last Modified_______: 2019-02-19
+OU ID_______________: 112928835
+OU EMAIL____________: bstick2@ou.edu
+
+Tasks:
+1. two token calculator
+2. three token calculator
+3. execute command
+4. parse and execute command
+5. calculator tests
+
+Estimated Time (minutes):
+1. 5
+2. 5
+3. 10
+4. 10
+5. 60
+
+Actual Time (minutes):
+1. 5
+2. 5
+3. 25
+4. 15
+5. 100
+*/}
